3590ba8c8f182b07ea9389cf539818aaacd64e1d
Jtolen543 <Jtolen543@gmail.com>
2025-10-29 20:00:23 -0400

    update

--- bin/init-app.js
+++ bin/init-app.js
@@ -19,6 +19,59 @@ const dest = path.resolve(process.cwd(), targetDir);
 const templateDir = path.join(__dirname, "..", "template");
 fs.cpSync(templateDir, dest, { recursive: true });
 
+// Ensure .gitignore exists in the created project
+const gitignorePath = path.join(dest, ".gitignore");
+const plainGitignorePath = path.join(dest, "gitignore");
+
+if (fs.existsSync(plainGitignorePath)) {
+  fs.renameSync(plainGitignorePath, gitignorePath);
+} else if (!fs.existsSync(gitignorePath)) {
+  const defaultGitignore = `# See https://help.github.com/articles/ignoring-files/ for more about ignoring files.
+
+# dependencies
+/node_modules
+/.pnp
+.pnp.*
+.yarn/*
+!.yarn/patches
+!.yarn/plugins
+!.yarn/releases
+!.yarn/versions
+
+# testing
+/coverage
+
+# next.js
+/.next/
+/out/
+
+# production
+/build
+
+# misc
+.DS_Store
+*.pem
+
+# debug
+npm-debug.log*
+yarn-debug.log*
+yarn-error.log*
+.pnpm-debug.log*
+
+# env files (can opt-in for committing if needed)
+.env*
+migrations/*
+
+# vercel
+.vercel
+
+# typescript
+*.tsbuildinfo
+next-env.d.ts
+`;
+  fs.writeFileSync(gitignorePath, defaultGitignore);
+}
+
 console.log("Installing dependencies...");
 execSync("npm install", { cwd: dest, stdio: "inherit" });
 
